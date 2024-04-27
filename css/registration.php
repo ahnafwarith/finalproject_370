d659affaffe2c72bacf26080e093863bc1eab8d0
ahnafwarith <corp.ahnafwarid@gmail.com>
2024-04-27 19:23:15 +0600

    finalized everything

---
+++ css/registration.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Registration</title>
+        <link rel="stylesheet" type="text/css" href="css/inventory.css">
+    </head>
+
+    <body id="RegBody">
+        <div class="container">
+            <div class="RegHeader">
+                <h3>SignUp</h3>
+            </div>
+            <div class="RegBody">
+                <form action="">
+                    <div class="RegInputsContainer">
+                        <label for="">Username</label>
+                        <input placeholder=" username" type="text" />
+                    </div>
+                    <div class="RegInputsContainer">
+                        <label for="">Password</label>
+                        <input placeholder= "password" type="password" />
+                    </div>
+                    <div class="RegButtonContainer">
+                        <button>Login</button>
+                    </div>
+                </form>
+
+            </div>
+
+    </body>
+</html>
